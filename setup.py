0e91127fcca760a3ebb845485f2eab000fbd0cf6
Adam Haney <adamhaney@users.noreply.github.com>
2018-06-04 11:13:50 -0400

    Fixing import for pip >= 10

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@
 """Package description
 """
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 from distutils.util import convert_path
 from fnmatch import fnmatchcase
 import os
 import sys
 import uuid
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 ###############################
 #  Configuración del paquete  #
